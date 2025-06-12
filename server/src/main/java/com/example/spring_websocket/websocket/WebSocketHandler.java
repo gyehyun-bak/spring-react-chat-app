96e6c9a5d8be171d455a954e50b67ae93e93c2bb
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-12 13:32:14 +0900

    feature: Added handling of SessionUnsubscribeEvent: member leaves chatroom

--- server/src/main/java/com/example/spring_websocket/websocket/WebSocketHandler.java
+++ server/src/main/java/com/example/spring_websocket/websocket/WebSocketHandler.java
@@ -1,13 +1,14 @@
 package com.example.spring_websocket.websocket;
 
+import com.example.spring_websocket.chatroom.ChatRoomService;
 import com.example.spring_websocket.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
 @Component
 @RequiredArgsConstructor
@@ -15,24 +16,40 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class WebSocketHandler {
 
     private final MemberService memberService;
+    private final ChatRoomService chatRoomService;
 
     /**
      * STOMP 서브 프로토콜을 사용하는 웹소켓 세션의 연결이 끊기면 발생하는 이벤트 {@link SessionDisconnectEvent}를 처리합니다.
      */
     @EventListener
     public void handleSessionDisconnect(SessionDisconnectEvent event) {
-        Long memberId = getMemberIdFromMessage(event.getMessage());
+        SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        Long memberId = getMemberIdFromHeaderAccessor(accessor);
 
         memberService.leave(memberId);
     }
 
-    private Long getMemberIdFromMessage(Message<byte[]> event) {
-        SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(event);
+    @EventListener
+    public void handleSessionUnsubscribe(SessionUnsubscribeEvent event) {
+        SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        Long memberId = getMemberIdFromHeaderAccessor(accessor);
+        Long chatRoomId = getChatRoomIdFromHeaderAccessor(accessor);
+        chatRoomService.leave(memberId, chatRoomId);
+    }
 
+    private Long getMemberIdFromHeaderAccessor(SimpMessageHeaderAccessor accessor) {
         if (accessor.getSessionAttributes() == null || !accessor.getSessionAttributes().containsKey("memberId")) {
             throw new IllegalStateException("No SessionAttributes or memberId. sessionId: " + accessor.getSessionId());
         }
 
         return (Long) accessor.getSessionAttributes().get("memberId");
+    }
+
+    private Long getChatRoomIdFromHeaderAccessor(SimpMessageHeaderAccessor accessor) {
+        if (accessor.getDestination() == null || !accessor.getDestination().startsWith("/topic/chat-rooms/")) {
+            throw new IllegalStateException("No destination or destination is not start with '/topic/chat-rooms/'. destination: " + accessor.getDestination());
+        }
+
+        return Long.parseLong(accessor.getDestination().replace("/topic/chat-rooms/", ""));
     }
 }
